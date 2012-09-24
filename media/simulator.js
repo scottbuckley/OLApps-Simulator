a7de8aa31e7dc698b71771ac199bf73a2ad42674
Scott Buckley <scott@buck.ly>
2012-09-25 00:47:48 +1000

    further parse structuring. preparing for instruction class definitions.

--- media/simulator.js
+++ media/simulator.js
@@ -23,9 +23,41 @@ var UnlockEditor = function() {
 	$("#editor_wrapper").removeClass("editor_disabled");
 };
 
+// instructions
+var InstructionList = ['stb', 'stba', 'sth', 'stha', 'st', 'sta', 'std', 'stda'     , 
+                       'ldsb', 'ldsba', 'ldsh', 'ldsha', 'ldub', 'lduba', 'lduh'    , 
+					   'lduha', 'ld', 'lda', 'ldd', 'ldda', 'ldstub', 'ldstuba'     , 
+					   'swap', 'swapa', 'sethi', 'add', 'addcc', 'addx', 'addxcc'   , 
+					   'and', 'andcc', 'andn', 'andncc', 'mulscc', 'or', 'orcc'     , 
+					   'orn', 'orncc', 'udiv', 'udivcc', 'umul', 'umulcc', 'sdiv'   , 
+					   'sdivcc', 'smul', 'smulcc', 'sub', 'subcc', 'subx', 'subxcc' , 
+					   'taddcc', 'taddcctv', 'tsubcc', 'tsubcctv', 'xor', 'xorcc'   , 
+					   'xnor', 'xnorcc', 'sll', 'sra', 'srl', 'bn', 'ba', 'be', 'bz', 
+					   'bne', 'bnz', 'ble', 'bg', 'bl', 'bge', 'bleu', 'bgu', 'bcs' , 
+					   'blu', 'bcc', 'bgeu', 'bneg', 'bpos', 'bvs', 'bvc', 'jmpl'   , 
+					   'rett', 'call', 'restore', 'save', 'tn', 'ta', 'te', 'tz'    , 
+					   'tne', 'tnz', 'tle', 'tg', 'tl', 'tge', 'tleu', 'tgu', 'tcs' , 
+					   'tlu', 'tcc', 'tgeu', 'tneg', 'tpos', 'tvs', 'tvc', 'nop'    , 
+					   'unimp', 'stbar', 'rd', 'wr', 'flush' ];
+
+// simple synthetics (single line -> single line)
+var SimpleSyntheticList = ['bclr', 'bset', 'btst', 'btog', 'call', 'clr', 'clrb', 
+                           'clrh', 'cmp', 'dec', 'deccc', 'inc', 'inccc', 'jmp' , 
+                           'mov', 'neg', 'not', 'restore', 'save', 'ret', 'retl', 
+                           'tst' ];
+
+// complex synthetics (single line -> multiple lines)
+var ComplexSyntheticList = ['set'];
+
+var getInstructionList      = function() { return InstructionList;      };
+var getSimpleSyntheticList  = function() { return SimpleSyntheticList;  };
+var getComplexSyntheticList = function() { return ComplexSyntheticList; };
+
+
 /* classes */
 
-
+// this class, whose constructor is given the raw text of a line of
+// code, parses said line into all the relevant 'words' within the line.
 function CodeLine(codeString, codeLine) {
 	this.originalCode = codeString;
 	this.lineNumber = codeLine;
@@ -74,30 +106,75 @@ function CodeLine(codeString, codeLine) {
 
 function RSim_SyntaxAnalysis() {
 	var aceEditSession = aceEditor.getSession();
-	var rawCodeLines = new Array();
-	var parsedLines = new Array();
-	var simpleLines = new Array();
 	var labelDefinitions = new Object();
+	var simpleSyntheticList  = getSimpleSyntheticList();
+	var complexSyntheticList = getComplexSyntheticList();
+	var instructionList      = getInstructionList();
 	
 	this.parse = function() {
-
+	
 		//get text (lines) from aceEditor object.
-		rawCodeLines = aceEditSession.getLines(0, aceEditSession.getLength())
+		var rawCodeLines = aceEditSession.getLines(0, aceEditSession.getLength())
 		
 		// convert raw code lines into CodeLine objects
-		//    rawCodeLines[] -> parsedLines[]
-		parsedLines = new Array();
-		for (rawLineIndex in rawCodeLines) {
-			thisLine = new CodeLine(rawCodeLines[rawLineIndex], (parseInt(rawLineIndex) + 1));
-			parsedLines.push(thisLine);
-		}
+		var parsedLines = parseRawLines(rawCodeLines);
 		
 		// convert CodeLine objects into SimpleCodeLine objects, omitting blank lines and
 		// creating a map of labels to line numbers.
-		//    parsedLines[] -> simpleLines[]
+		var simpleLines = processCodeLines(parsedLines);
+		
+		
+		
+		
+		
+		
+		
+		/** START DEBUG SECTION **/
+		
+			// debug
+			$("#debugbox").text(""); //debug
+			for (i in simpleLines) {
+				thisLine = simpleLines[i];
+				//$("#debugbox").append();
+				$("#debugbox").append('{' + thisLine.lineNumber + '}{');
+				$("#debugbox").append(thisLine.instruction + '}{');
+				$("#debugbox").append(thisLine.parameters + '}{');
+				$("#debugbox").append('<br /><br/>');
+			}
+			
+			$("#debugbox").append('<br /><br/>');
+			
+			for (key in labelDefinitions) {
+				if (labelDefinitions.hasOwnProperty(key)) {
+					$("#debugbox").append('{' + key + ': ' + labelDefinitions[key] + '}');
+					$("#debugbox").append('<br />');
+				}
+			}
+		
+		/**  END DEBUG SECTION  **/
+		
+		
+		
+	};
+	
+	// converts raw code lines into CodeLine objects
+	var parseRawLines = function(rawCodeLines) {
+		parsedLines = new Array();
+		for (rawLineIndex in rawCodeLines) {
+			var thisLine = new CodeLine(rawCodeLines[rawLineIndex], (parseInt(rawLineIndex) + 1));
+			parsedLines.push(thisLine);
+		}
+		return parsedLines;
+	};
+	
+	// converts a list of CodeLine objects into a list of SimpleCodeLine objects,
+	// also fills the private class attribute labelDefinitions object with label
+	// to line maps.
+	var processCodeLines = function(parsedLines) {
+		simpleLines = new Array();
 		var hasWaitingLabel = false;
 		var waitingLabels = new Array();
-		simpleLines = new Array();
+		
 		for (parsedLineIndex in parsedLines) {
 			var thisLine = parsedLines[parsedLineIndex];
 			
@@ -110,51 +187,49 @@ function RSim_SyntaxAnalysis() {
 						// this line has an instruction. add it to the list.
 						simpleLines.push(new SimpleCodeLine(thisLine.instruction, thisLine.parameters, thisLine.lineNumber));
 						
-						// add this line's label to the list.
 						if (thisLine.hasLabel === true) {
-							//labelDefinitions.push(new LabelDefinition(thisLine.label, thisLine.lineNumber));
+							// add this line's label to the list.
 							labelDefinitions[thisLine.label] = thisLine.lineNumber;
 						}
 						
-						// add any previously defined labels to the list for this line number.
 						if (hasWaitingLabel === true) {
+							// add any previously defined labels to the list for this line number.
 							for (waitingLabelIndex in waitingLabels) {
-								//labelDefinitions.push(new LabelDefinition(waitingLabels[waitingLabelIndex], thisLine.lineNumber));
 								labelDefinitions[waitingLabels[waitingLabelIndex]] = thisLine.lineNumber;
 							}
-							hasWaitingLabel = false;
 							waitingLabels = new Array();
+							hasWaitingLabel = false;
 						}
 					}
 			}
 		}
 		
-		// debug
-		$("#debugbox").text(""); //debug
-		for (i in simpleLines) {
-			thisLine = simpleLines[i];
-			//$("#debugbox").append();
-			$("#debugbox").append('{' + thisLine.lineNumber + '}{');
-			$("#debugbox").append(thisLine.instruction + '}{');
-			$("#debugbox").append(thisLine.parameters + '}{');
-			$("#debugbox").append('<br /><br/>');
-		}
+		return simpleLines;
+	}
+	
+	
+	var createInstructionObjects = function(simpleLines) {
+		var instructionObjects = new Array();
 		
-		$("#debugbox").append('<br /><br/>');
-		
-		for (key in labelDefinitions) {
-			if (labelDefinitions.hasOwnProperty(key)) {
-				$("#debugbox").append('{' + key + ': ' + labelDefinitions[key] + '}');
-				$("#debugbox").append('<br />');
+		for (simpleLineIndex in simpleLine) {
+			thisLine = simpleLines[simpleLineIndex];
+			
+			/** Instructions **/ 
+			var whatever = "debug";
+			instructionObjects.push(whatever);
+			
+			/** Simple Synthetics **/
+			instructionObjects.push(whatever);
+			
+			/** Complex Synthetics **/
+			var whatevers = whatever;
+			for (whatevers in whatever) {
+				instructionObjects.push(whatevers);
 			}
+			
 		}
-		
-		
-		
 		
 	};
-	
-	var 
 	
 }
 
